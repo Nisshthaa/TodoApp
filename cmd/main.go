66023d67cb91f5bf84b0e16233624ae87b44bbf3
nisshthaa <sharmanishtha791@gmail.com>
2026-06-26 12:08:30 +0530

    Initial project setup

---
+++ cmd/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/yourusername/my-project/database"
+)
+
+func main() {
+	if err := database.OpenConnection(
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+	); err != nil {
+		log.Panicf("Failed to initialize and migrate database with error: %v", err)
+	}
+
+	defer database.Todo.Close()
+
+	router := chi.NewRouter()
+
+	if err := http.ListenAndServe(":8080", router); err != nil {
+		log.Panicf("Failed to start server with error: %v", err)
+
+	}
+	log.Println("Server started at :8080")
+
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/yourusername/my-project
+
+go 1.26.3
+
+require (
+	github.com/go-chi/chi/v5 v5.3.0 // indirect
+	github.com/jmoiron/sqlx v1.4.0 // indirect
+	github.com/lib/pq v1.12.3 // indirect
+	golang.org/x/crypto v0.53.0 // indirect
+)
